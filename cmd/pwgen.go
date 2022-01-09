f2f2fcb76047b8ed34123e49c8bd4719e2b52a95
Vitaliy Vasilenko <me@vitalvas.com>
2022-01-09 10:05:19 +0200

    add str chars only mode

--- cmd/pwgen.go
+++ cmd/pwgen.go
@@ -14,17 +14,24 @@ var pwgenCmd = &cobra.Command{
 
 var pwgenLen int
 var pwgenIPMI bool
+var pwgenStr bool
 
 func init() {
 	rootCmd.AddCommand(pwgenCmd)
 
 	pwgenCmd.PersistentFlags().IntVar(&pwgenLen, "len", 24, "Password length")
 	pwgenCmd.PersistentFlags().BoolVar(&pwgenIPMI, "ipmi", false, "Password for IPMI")
+	pwgenCmd.PersistentFlags().BoolVarP(&pwgenStr, "str", "s", false, "String chars only")
 }
 
 func pwgen(cmd *cobra.Command, args []string) {
 	specialCharsLen := 2
 	numLen := 3
+
+	if pwgenStr {
+		specialCharsLen = 0
+		numLen = 0
+	}
 
 	if pwgenIPMI {
 		// Can be 16 or 20 bytes in ASCII length. In real - 15 or 19 chars
